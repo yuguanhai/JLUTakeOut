0936efa7cf3630eee41864a250768a699b7eb824
yuguanhai <76604035+yuguanhai@users.noreply.github.com>
2024-04-01 21:56:56 +0800

    Update SkyApplication.java

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+aaa
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @EnableCaching //开启缓存功能
